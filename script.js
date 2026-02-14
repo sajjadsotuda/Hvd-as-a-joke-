80a3e5dea5ed85289707129e4c17060f2ac2d015
Sajjad <sajjadsotuuda@gmail.com>
2026-02-14 10:45:51 +0430

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const actionArea = document.getElementById('action-area');
 const question = document.getElementById('question');
+const socialArea = document.getElementById('social-area');
 
-// 1. Falling Hearts Shower
+// 1. Falling Hearts
 function createHeart() {
     const heart = document.createElement('div');
     heart.classList.add('heart-particle');
@@ -10,21 +11,17 @@ function createHeart() {
     heart.style.fontSize = Math.random() * 20 + 15 + "px";
     const duration = Math.random() * 3 + 3;
     heart.style.animationDuration = duration + "s";
-    heart.style.opacity = Math.random() * 0.7 + 0.3;
-
     document.body.appendChild(heart);
     setTimeout(() => heart.remove(), duration * 1000);
 }
-
 setInterval(createHeart, 300);
 
-// 2. The "No" logic - Show Try Again button
+// 2. No Button Logic
 function askAgain() {
     question.innerHTML = "Wait, did you misclick? <br>Let's try that one more time... 🥺";
     actionArea.innerHTML = `<button class="reset-btn" onclick="resetStage()">Try again 🥺</button>`;
 }
 
-// 3. Reset logic - Go back to first stage
 function resetStage() {
     question.innerHTML = "Will you be my Valentine? ❤️";
     actionArea.innerHTML = `
@@ -33,12 +30,13 @@ function resetStage() {
     `;
 }
 
-// 4. Success logic - Flirty Response
+// 3. Yes Button Logic
 function celebrate() {
-    question.innerHTML = "My hbs and I only built a whole website as a 'joke'... <br>but I'm glad you're down to be my <br>favorite long-distance Valentine. 😉❤️";
-    actionArea.innerHTML = ""; // Removes buttons
+    question.innerHTML = "I only built a whole website as a 'joke'... <br>but I'm glad you're down to be my Valentine.❤️";
 
-    // Victory Heart Burst
+    actionArea.style.display = "none";
+    socialArea.style.display = "block";
+
     for (let i = 0; i < 50; i++) {
         setTimeout(createHeart, i * 50);
     }
